5c0a45647b5a1c09cef4c9f879dae77d9d457942
dengyansen <739049105@qq.com>
2020-08-21 16:25:20 +0800

    study

--- src/main/java/concurrent/ThreadLocalData.java
+++ src/main/java/concurrent/ThreadLocalData.java
@@ -6,19 +6,19 @@ import java.util.concurrent.CountDownLatch;
 
 public class ThreadLocalData {
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-        UserRequest innerClass = new UserRequest();
-        for(int i = 1; i <= 2; i++) {
-            new Thread(() -> {
-                for(int j = 0; j < 4; j++) {
-                    SessionHandler.setUser(Thread.currentThread().getName());
-                    System.out.println(SessionHandler.get());
-                }
-//                innerClass.set("hello world");
-                countDownLatch.countDown();
-            }, "thread - " + i).start();
-        }
-        countDownLatch.await();
+//        CountDownLatch countDownLatch = new CountDownLatch(2);
+//        UserRequest innerClass = new UserRequest();
+//        for(int i = 1; i <= 2; i++) {
+//            new Thread(() -> {
+//                for(int j = 0; j < 4; j++) {
+//                    SessionHandler.setUser(Thread.currentThread().getName());
+//                    System.out.println(SessionHandler.get());
+//                }
+////                innerClass.set("hello world");
+//                countDownLatch.countDown();
+//            }, "thread - " + i).start();
+//        }
+//        countDownLatch.await();
     }
     static class UserRequest implements Runnable{
         public void run() {
